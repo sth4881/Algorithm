a1b79fb6cad753b6ada2b4988ed8b51745b77826
sth4881 <sth4881@naver.com>
2021-08-04 20:49:44 +0900

    [Add] BOJ1697.java 추가

--- Baekjoon Online Judge/Silver/BOJ1697.java
+++ Baekjoon Online Judge/Silver/BOJ1697.java
@@ -10,28 +10,27 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class BOJ1697 {
-	public static int[] dx = { -1, 1, 2 };
+	public static int[] dx = { -1, 1, 2 }; // 왼쪽 또는 오른쪽으로 한 칸 이동하거나 x2만큼 순간이동 가능
 	public static void bfs(int start, int end, int[] check) {
-		Queue<Integer> q = new LinkedList<Integer>();
-		check[start] = 0;
+		Queue<Integer> q = new LinkedList<Integer>(); // BFS를 사용하기 위해서 큐를 생성
+		check[start] = 0; // 시작점에서는 움직이지 않으므로 소요되는 시간도 0초
 		
-		q.offer(start);
+		q.offer(start); // 시작점을 큐에 넣어주는것으로 BFS 알고리즘 시작
 		while(!q.isEmpty()) {
-			int nx;
-			int vtx = q.poll();
+			int nx; // 다음에 방문할 정점을 받기 위한 변수 선언
+			int vtx = q.poll(); // 다음 방문할 정점을 탐색하기 위해서 큐에 들어있는 값들을 하나씩 꺼내옴 
 			for(int i=0; i<3; i++) {
-				if(dx[i]==-1 || dx[i]==1) {
+				if(dx[i]==-1 || dx[i]==1) { // 왼쪽 또는 오른쪽으로 한 칸씩 이동하는 경우에 대해서는 덧셈 수행
 					nx = vtx+dx[i];
-				} else {
+				} else { // x2만큼 순간이동하는 경우에 대해서는 곱셈 수행
 					nx = vtx*dx[i];
 				}
-
-				if(nx>=0 && nx<check.length && check[nx]==0) {
-					check[nx] = check[vtx]+1;
-					q.offer(nx);
+				if(nx>=0 && nx<check.length && check[nx]==0) { // 다음으로 방문할 정점 nx가 배열의 범위를 벗어나지 않았고, 방문되지 않았을 경우
+					check[nx] = check[vtx]+1; // 최소시간을 구하기 위해서 다음 정점에 현재 정점+1
+					q.offer(nx); // nx를 큐에 추가
 				}
-				if(nx==end) {
-					System.out.println(check[nx]); // 시작점에서 출발점에 도착한 경우 걸린 최소시간 반환
+				if(nx==end) { // 시작점에서 출발점에 도착한 경우 위의 과정을 중단하고 걸린 최소시간을 반환
+					System.out.println(check[nx]); 
 					return;
 				}
 			}
@@ -45,12 +44,11 @@ public class BOJ1697 {
 			String[] tmp = br.readLine().split(" ");
 			int start = Integer.parseInt(tmp[0]);
 			int end = Integer.parseInt(tmp[1]);
+
+			int[] check = new int[100001]; // 목표에서 요구하는 최소시간을 구하기 위한 배열
 			
-			int size = Math.max(start, end);
-			int[] check = new int[100001];
-			
-			if(start==end) bw.write(0+"\n");
-			else bfs(start, end, check);
+			if(start==end) bw.write(0+"\n"); // start와 end가 같은 경우에 대해서는 0을 출력
+			else bfs(start, end, check); // 그 외의 경우에 대해서는 BFS 알고리즘 수행
 
 			bw.close();
 		} catch(FileNotFoundException e) {
