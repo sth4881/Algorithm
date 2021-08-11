5794081121229d4ea391f73e6a146d6793fc1ed8
Jinho Song <46771903+sth4881@users.noreply.github.com>
2021-08-11 22:51:55 +0900

    Update Basic_Quiz4.java

--- Code Tree/intermediate-low-solved/Quiz/Basic_Quiz4.java
+++ Code Tree/intermediate-low-solved/Quiz/Basic_Quiz4.java
@@ -27,13 +27,13 @@ public class Basic_Quiz4 {
 				for(int j=0; j<seq.length; j++) {
 					String target = seq[j]; //  j번째 이후의 원소들에 대해서 같은지를 비교하기 위해서 i-1번째 수열의 j번째 원소를 타겟으로 삼음
 					int cnt = 1; // 타겟으로 삼은 원소에 대한 반복을 위해서 cnt를 1로 초기화
-                    while(j<seq.length-1 && target.equals(seq[j+1])) { // 타겟으로 삼은 원소의 값과 j+1 원소의 값이 같다면
-                        cnt++;
-                        j++;
-                    }
-                    // 타겟으로 삼은 원소의 값과 j+1 원소의 값이 같지 않으면 i번째 수열의 값을 target과 cnt값을 더해서 갱신
-                    // 마지막 원소와 그 전 원소의 값이 같은 경우에는 while문을 돌고, j가 증가하면서 for문을 빠져나옴 
-                    // 마지막 원소와 그 전 원소의 값이 다른 경우에는 while문을 돌지 않고 빠져나옴
+					while(j<seq.length-1 && target.equals(seq[j+1])) { // 타겟으로 삼은 원소의 값과 j+1 원소의 값이 같다면
+						cnt++;
+						j++;
+					}
+					// 타겟으로 삼은 원소의 값과 j+1 원소의 값이 같지 않으면 i번째 수열의 값을 target과 cnt값을 더해서 갱신
+					// 마지막 원소와 그 전 원소의 값이 같은 경우에는 while문을 돌고, j가 증가하면서 for문을 빠져나옴 
+					// 마지막 원소와 그 전 원소의 값이 다른 경우에는 while문을 돌지 않고 빠져나옴
 					arr[i] += (target+Integer.toString(cnt));
 				}
 			}
