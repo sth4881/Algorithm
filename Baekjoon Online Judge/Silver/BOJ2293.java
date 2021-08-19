169a662253427e1b4ebddfca5525e77afad9bf48
Jinho Song <sth4881@naver.com>
2021-08-19 20:57:46 +0900

    [Update] BOJ2293.java 재풀이 완료

--- Baekjoon Online Judge/Silver/BOJ2293.java
+++ Baekjoon Online Judge/Silver/BOJ2293.java
@@ -13,22 +13,21 @@ public class BOJ2293 {
 			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 			
-			String[] tmp = br.readLine().split(" ");
-			int n = Integer.parseInt(tmp[0]);
-			int k = Integer.parseInt(tmp[1]);
+			String[] input = br.readLine().split(" ");
+			int n = Integer.parseInt(input[0]);
+			int k = Integer.parseInt(input[1]);
 			
-			int[] arr = new int[n];
-			for(int i=0; i<n; i++) {
-				arr[i] = Integer.parseInt(br.readLine());
-			}
+			int[] coin = new int[k];
+			for(int i=0; i<n; i++)
+				coin[i] = Integer.parseInt(br.readLine());
 			
 			int[] dp = new int[k+1];
-			dp[0] = 1; // 동전을 아예 사용하지 않는 경우도 경우의 수라고 생각한다.
-			for(int i=0; i<n; i++) {
-				for(int j=arr[i]; j<=k; j++) { // 사용한 동전의 구성이 같은데, 순서만 다른 것은 같은 경우를 제외하기 위해서 j값을 매번 i번째 동전으로 초기화
-					dp[j] += dp[j-arr[i]]; // dp[j]는 첫번째부터 j번째까지의 동전을 사용했을 때의 합이 k원이 되도록 만드는 경우의 수이다.
+			dp[0] = 1;
+			for(int i=0; i<n; i++) { // 동전 N개에 대해서
+				for(int j=1; j<=k; j++) { // 1부터 K까지의 경우에 대해서
+					if(j>=coin[i]) dp[j] += dp[j-coin[i]]; // i번째 동전이 가치 j와 값이 같거나 작은 경우, 둘의 차를 뺐을 때까지의 경우의 수를 대입
 				}
-			}
+			}		
 			
 			bw.write(dp[k]+"\n");
 			bw.close();
