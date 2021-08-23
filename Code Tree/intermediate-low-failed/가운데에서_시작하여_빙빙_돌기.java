30a6a35ec35bde95879d55e7077dc3bd448242c8
Jinho Song <sth4881@naver.com>
2021-08-24 00:57:24 +0900

    [Add][Basic] 대각선으로_숫자_채우기.java 풀이 미완료

--- Code Tree/intermediate-low-failed/가운데에서_시작하여_빙빙_돌기.java
+++ Code Tree/intermediate-low-failed/가운데에서_시작하여_빙빙_돌기.java
@@ -14,18 +14,7 @@ public class 가운데에서_시작하여_빙빙_돌기 {
 	public static void func(int[][] grid, int cx, int cy, int n) {
 		int num = 1;
 		grid[cy][cx] = num; // 시작 위치의 값을 1로 초기화
-		for(int i=1; i<n*n; i++) { // n*n-1개의 칸을 채우기 위해서
-			while(true) { // 이동할 때까지 방향을 바꿔주기 위해서 while문 삽입
-				int nx = cx + dx[dir];
-				int ny = cy + dy[dir];
-				if(nx>=0 && ny>=0 && nx<n && ny<n && grid[ny][nx]==0) { // 범위 이탈 & 이동하려는 위치의 값이 0이면
-					grid[ny][nx] = num+1; // 다음으로 이동하려는 위치의 값을 현재 값 +1로 변경
-					cx = nx; cy = ny; // 현재 x와 현재 y값을 다음 x와 다음 y값으로 갱신 
-					num++;
-					break; // 다음 칸으로 이동하기 위해서 break
-				} else dir = (dir+1) % 4; // 그렇지 않으면 방향을 바꿔준다.
-			}
-		}
+		
 	}
 	public static void main(String[] args) {
 		try {
