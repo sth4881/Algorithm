a1101e3aa9f75ec5857d6062bc75144c6ac74d97
Jinho Song <sth4881@naver.com>
2021-08-24 14:59:26 +0900

    [Update][Basic] 대각선으로 숫자 채우기 풀이 완료

--- Code Tree/intermediate-low-solved/Basic/대각선으로_숫자_채우기.java
+++ Code Tree/intermediate-low-solved/Basic/대각선으로_숫자_채우기.java
@@ -8,31 +8,11 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 public class 대각선으로_숫자_채우기 {
-	public static int[] dx = { -1 };
-	public static int[] dy = { 1 };
-	public static void func(int[][] grid, int cx, int cy, int n, int m) {
-		int num = 1;
-		int col_idx = 0;
-		int row_idx = 0;
-		grid[0][0] = num;
-		for(int i=1; i<n*m; i++) {
-			int nx = cx-1;
-			int ny = cy+1;
-			if(nx>=0 && ny>=0 && nx<m && ny<n) { // 범위 이탈 방지
-				cx = nx; cy = ny;
-				grid[cy][cx] = ++num;
-			}
-			 else { // 범위를 이탈한 경우 첫번째 행으로 이동하고 열은 col+1로 이동
-				if(col_idx<m-1) { // 마지막 열 전까지에 대해서
-					cx = ++col_idx; cy = 0;
-					grid[cy][cx] = ++num;
-				} else { // 마지막 열에 대해서
-					if(row_idx<n-1) { // 마지막 행 전까지에 대해서
-						cx = n-1; cy = ++row_idx;
-						grid[cy][cx] = ++num;
-					}
-				}
-			 }
+	public static int cnt = 1;
+	public static void func(int[][] grid, int row, int col, int n) {
+		while(col>=0 && row<n) { // 범위 이탈 방지
+			grid[row][col] = cnt++;
+			row++; col--;
 		}
 	}
 	public static void main(String[] args) {
@@ -45,7 +25,10 @@ public class 대각선으로_숫자_채우기 {
 			int m = Integer.parseInt(input[1]);
 			int[][] grid = new int[n][m];
 			
-			func(grid, 0, 0, n, m);
+			for(int col=0; col<m; col++) // 첫번째 행의 모든 열에 대해서 수행
+				func(grid, 0, col, n);
+			for(int row=1; row<n; row++) // 마지막 열의 모든 행에 대해서 수행
+				func(grid, row, m-1, n);
 			
 			for(int i=0; i<n; i++) {
 				for(int j=0; j<m; j++)
