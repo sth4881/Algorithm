51b608828c2c3f7d6a75c61c42020a7eecc71a73
sth4881 <sth4881@naver.com>
2021-09-15 16:28:07 +0900

    [Update][Basic] Run_Length_인코딩.java 코드 리팩토링

--- Code Tree/intermediate-low-solved/Basic/Run_Length_인코딩.java
+++ Code Tree/intermediate-low-solved/Basic/Run_Length_인코딩.java
@@ -1,4 +1,4 @@
-package com.algorithm.codetree;
+package com.algorithm.codetree.basic;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -20,17 +20,14 @@ public class Run_Length_인코딩 {
 			int cnt = 1;
 			for(int i=0; i<input.length-1; i++) {
 				if(input[i].equals(input[i+1])) cnt++; // i번째 문자열과 i+1번째 문자열이 일치하면 cnt 1 증가
-				else {
-					list.add(input[i]); // 일치하지 않으면 i번째 문자열을 list에 추가
-					list.add(Integer.toString(cnt)); // 일치하지 않으면 cnt를 list에 추가
+				else { // 일치하지 않으면 i번째 문자열과 cnt를 list에 차례대로 추가
+					list.add(input[i]);
+					list.add(Integer.toString(cnt));
 					cnt=1; // cnt를 재사용하기 위해서 초기화
 				}
 			}
-			
-			// 마지막 문자열에 대해서 처리
-			list.add(input[input.length-1]);
-			if(input.length>1 && !input[input.length-1].equals(input[input.length-2])) list.add("1");
-			else list.add(Integer.toString(cnt));
+			list.add(input[input.length-1]); // 마지막 문자열은 i-1번째 문자열과 일치하든 안하든 list에 추가
+			list.add(Integer.toString(cnt)); // 위의 for문을 빠져나와도 cnt 값은 변하지 않으므로 list에 그대로 추가
 			
 			String answer = "";
 			for(String item : list)
