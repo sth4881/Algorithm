ba8c5f4ebd60cd08a50fa0cc9b32be6c020de052
sth4881 <sth4881@naver.com>
2021-11-15 18:39:23 +0900

    [Solve] 👨‍💻 BOJ5212_지구온난홮 풀이 완료

--- Baekjoon Online Judge/Silver/src/[BOJ]5212_지구온난화.java
+++ Baekjoon Online Judge/Silver/src/[BOJ]5212_지구온난화.java
@@ -12,18 +12,13 @@ public class BOJ5212 {
 	public static void makeMap(int row, int col) {
 		for(int i=1; i<row+1; i++) {
 			for(int j=1; j<col+1; j++) {
-				int cnt = 0;
-				if(island[i][j]=='.') continue;
-				// 상
-				if(island[i-1][j]=='.') cnt++;
-				// 하
-				if(island[i+1][j]=='.') cnt++;
-				// 좌
-				if(island[i][j-1]=='.') cnt++;
-				// 우
-				if(island[i][j+1]=='.') cnt++;
-				// cnt가 3이상이라면 해당 위치의 X값을 '.'으로 변경
-				if(cnt>=3) newIsland[i][j] = '.';
+				int cnt = 0; // 바다와 인접한 방향의 개수를 찾기 위한 변수 cnt
+				if(island[i][j]=='.') continue; // 현재 위치가 섬이 아닌 바다인 경우 continue
+				if(island[i-1][j]=='.') cnt++; // 섬의 윗 부분이 바다와 인접한 경우
+				if(island[i+1][j]=='.') cnt++; 	// 섬의 아랫 부분이 바다와 인접한 경우
+				if(island[i][j-1]=='.') cnt++; // 섬의 좌측 부분이 바다와 인접한 경우
+				if(island[i][j+1]=='.') cnt++; // 섬의 우측 부분이 바다와 인접한 경우
+				if(cnt>=3) newIsland[i][j] = '.'; // cnt가 3이상이라면 해당 위치의 X값을 '.'으로 변경
 			}
 		}
 	}
@@ -32,12 +27,21 @@ public class BOJ5212 {
 			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+			// 입력
 			String[] input = br.readLine().split(" ");
 			int r = Integer.parseInt(input[0]);
 			int c = Integer.parseInt(input[1]);
 			
+			// 패딩을 이용하여 바다의 면적을 증가
 			island = new char[r+2][c+2];
 			newIsland = new char[r+2][c+2];
+			// 패딩을 통해서 만들어진 바다를 '.'으로 표시
+			for(int i=0; i<r+2; i++) {
+				for(int j=0; j<c+2; j++) {
+					island[i][j] = newIsland[i][j] =  '.';
+				}
+			}
+			// 입력받은 지도를 통해서 섬의 위치를 'X'로 표시
 			for(int i=1; i<r+1; i++) {
 				String map = br.readLine();
 				for(int j=1; j<c+1; j++) {
@@ -45,7 +49,10 @@ public class BOJ5212 {
 				}
 			}
 			
+			// 50년 후의 남아있는 섬을 지도에 표시하는 알고리즘
 			makeMap(r, c);
+			// 한 행 또는 한 열이 바다로 이뤄지면 지도에서
+			// 없어지므로 정답을 출력하기 위한 지도를 재구성
 			int minR, maxR, minC, maxC;
 			minR = minC = Integer.MAX_VALUE;
 			maxR = maxC = Integer.MIN_VALUE;
@@ -59,9 +66,10 @@ public class BOJ5212 {
 					}
 				}
 			}
-
-			for(int i=minR; i<maxR; i++) {
-				for(int j=minC; j<maxC; j++)
+			
+			// 출력
+			for(int i=minR; i<=maxR; i++) {
+				for(int j=minC; j<=maxC; j++)
 					bw.write(newIsland[i][j]+"");
 				bw.write("\n");
 			}
