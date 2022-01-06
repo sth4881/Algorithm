e0b6b6865e78168f545ee1e392de6be45fc3a798
sth4881 <sth4881@naver.com>
2022-01-06 18:12:41 +0900

    [Solve] 👨‍💻 PRGMS42576_완주하지못한선수

---
+++ Programmers/Level1/src/PRGMS42576.java
@@ -0,0 +1,20 @@
+package com.algorithm.programmers;
+
+import java.util.HashMap;
+
+// 완주하지 못한 선수 - 해시
+class PRGMS42576 {
+    public String solution(String[] participant, String[] completion) {
+        HashMap<String, Integer> map = new HashMap<>();
+        for(String name : participant) {
+            if(!map.containsKey(name)) map.put(name, 1);
+            else map.put(name, map.get(name)+1);
+        }
+        for(String name : completion) {
+            int count = map.get(name);
+            if(count==1) map.remove(name);
+            else map.put(name, count-1);
+        }
+        return map.keySet().toString().replace("[", "").replace("]", "");
+    }
+}
